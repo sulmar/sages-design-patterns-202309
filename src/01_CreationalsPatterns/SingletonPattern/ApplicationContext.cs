eea18e1cb03db7f1c8ae8ea512fd65db86a417a4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-11 10:30:39 +0200

    Dodanie ApplicationContext

---
+++ src/01_CreationalsPatterns/SingletonPattern/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class ApplicationContext : Singleton<ApplicationContext>
+    {
+        public string LoggedUser { get; set; }
+        public DateTime LoggedOn { get; set; }
+    }
+}

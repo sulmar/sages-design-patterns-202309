1055cc74c1e6e5c045b682cea9db02d4612b56e6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-13 11:55:37 +0200

    Refaktoring

--- src/03_BehavioralsPatterns/TemplateMethodPattern/HappyHoursFixedOrderCalculator.cs
+++ src/03_BehavioralsPatterns/TemplateMethodPattern/HappyHoursFixedOrderCalculator.cs
@@ -2,22 +2,17 @@
 
 namespace TemplateMethodPattern
 {
-
-
-    // Happy Hours - 10% upustu w godzinach od 9 do 15
-    public class HappyHoursPercentageOrderCalculator : PercentageOrderCalculator
+    public class HappyHoursFixedOrderCalculator : FixedOrderCalculator
     {
         private readonly TimeSpan from;
         private readonly TimeSpan to;
 
-        public HappyHoursPercentageOrderCalculator(TimeSpan from, TimeSpan to, decimal percentage)
-                : base(percentage)
+        public HappyHoursFixedOrderCalculator(TimeSpan from, TimeSpan to, decimal amount) : base(amount)
         {
             this.from = from;
             this.to = to;
         }
 
         public override bool CanDiscount(Order order) => order.OrderDate.TimeOfDay >= from && order.OrderDate.TimeOfDay < to;
-        
     }
 }

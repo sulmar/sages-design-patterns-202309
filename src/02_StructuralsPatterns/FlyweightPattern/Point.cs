372aac868deb0716fd6f50ae27af1a17ba584e66
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-13 08:21:33 +0200

    Przeniesienie klas do osobnych plików

--- src/02_StructuralsPatterns/FlyweightPattern/Point.cs
+++ src/02_StructuralsPatterns/FlyweightPattern/Point.cs
@@ -22,19 +22,6 @@ namespace FlyweightPattern
         }
     }
 
-    // Flyweight
-    public class PointIcon
-    {
-        public PointType Type { get; set; }
-        public byte[] Icon { get; set; }
-
-        public PointIcon(PointType type, byte[] icon)
-        {
-            this.Type = type;
-            this.Icon = icon;
-        }
-    }
-
     public enum PointType
     {
         Coffee,

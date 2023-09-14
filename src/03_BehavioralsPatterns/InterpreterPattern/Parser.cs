7fd2f889de1562fe50b04b3dd3de49fa70a73f1d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-14 16:27:26 +0200

    Dodanie przykładu

--- src/03_BehavioralsPatterns/InterpreterPattern/Parser.cs
+++ src/03_BehavioralsPatterns/InterpreterPattern/Parser.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections;
 
 namespace InterpreterPattern
 {
     public class Parser
     {
+        // 2 * (3 + 1)
+
+        // 2 3 1 + *
         public int Evaluate(string s)
         {
             throw new NotImplementedException();
